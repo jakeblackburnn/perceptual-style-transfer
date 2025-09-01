7e21591d30568ff86d1b344525b6444850677de8
John Blackburn <jackblackburnn@icloud.com>
2025-09-01 09:27:29 -0400

    Add cuda device option

--- build.py
+++ build.py
@@ -5,17 +5,19 @@ from style_transfer.train import train_model
 
 if __name__ == '__main__':
 
-    model_name = 'dry_run'
+    model_name = 'medium_no_style_deep_loss_v1'
     model_config = Models.get(model_name)
 
     # Device setup
     print("PyTorch version:", torch.__version__)
-    print("MPS built:", torch.backends.mps.is_built())
-    print("MPS available:", torch.backends.mps.is_available())
+
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
         device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
     else:
         device = torch.device("cpu")
+
     print("using device:", device)
 
     train_model(model_name, model_config, device)
