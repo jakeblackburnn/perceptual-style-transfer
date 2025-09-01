eacd2e41ef71a4d44d0196d12cef4b77e4cede71
John Blackburn <jackblackburnn@icloud.com>
2025-09-01 11:19:37 -0400

    Add more model configs

--- style_transfer/config.py
+++ style_transfer/config.py
@@ -8,6 +8,7 @@ med_rate  = 5e-4
 high_rate = 1e-3
 
 #style weights
+extra_high_style = 2e5
 high_style = 8e4
 med_style  = 4e4
 low_style  = 2e4
@@ -16,7 +17,7 @@ low_style  = 2e4
 Models = {
     "dry_run": {
         "model_size": "small",
-        "loss": "standard",
+        "layer_preset": "standard",
 
         "content": {
             "dataset": "images/VOC2012",
@@ -38,7 +39,7 @@ Models = {
 
     "big_no_style_v1": {
         "model_size": "big",
-        "loss": "standard",
+        "layer_preset": "standard",
 
         "content": {
             "dataset": "images/VOC2012",
@@ -66,7 +67,34 @@ Models = {
 
     "medium_no_style_v1": {
         "model_size": "medium",
-        "loss": "standard",
+        "layer_preset": "standard",
+
+        "content": {
+            "dataset": "images/VOC2012",
+            "fraction": 0.02,
+        },
+
+        "style": {
+            "dataset": "images/Ukiyo_e",
+            "fraction": 0.01,
+        },
+
+        "curriculum": {
+            "stages": [
+                {
+                    "epochs": 6, 
+                    "lr": med_rate, 
+                    "content_batch_size": 4, 
+                    "style_batch_size": 1, 
+                    "style_weight": 0
+                },
+            ]
+        }
+    },
+
+    "medium_no_style_deep_loss_v1": {
+        "model_size": "medium",
+        "layer_preset": "deep",
 
         "content": {
             "dataset": "images/VOC2012",
@@ -93,7 +121,7 @@ Models = {
 
     "small_no_style_v1": {
         "model_size": "small",
-        "loss": "standard",
+        "layer_preset": "standard",
 
         "content": {
             "dataset": "images/VOC2012",
@@ -120,7 +148,7 @@ Models = {
 
     "basic_small_impressionism_v1": {
         "model_size": "small",
-        "loss": "standard",
+        "layer_preset": "standard",
 
         "content": {
             "dataset": "images/VOC2012",
@@ -135,6 +163,69 @@ Models = {
         "curriculum": {
             "stages": [
                 {"epochs": 4, "lr": low_rate},
+            ]
+        }
+    },
+
+    "basic_small_impressionism_deep_v1": {
+        "model_size": "small",
+        "layer_preset": "deep",
+
+        "content": {
+            "dataset": "images/VOC2012",
+            "fraction": 0.01,
+        },
+
+        "style": {
+            "dataset": "images/Impressionism",
+            "fraction": 0.01,
+        },
+
+        "curriculum": {
+            "stages": [
+                {"epochs": 4, "lr": low_rate, "style_weight": low_style },
+            ]
+        }
+    },
+
+    "medium_impressionism_weighted_v1": {
+        "model_size": "medium",
+        "layer_preset": "standard_weighted",
+
+        "content": {
+            "dataset": "images/VOC2012",
+            "fraction": 0.01,
+        },
+
+        "style": {
+            "dataset": "images/Impressionism",
+            "fraction": 0.01,
+        },
+
+        "curriculum": {
+            "stages": [
+                {"epochs": 4, "lr": low_rate, "style_weight": low_style},
+            ]
+        }
+    },
+
+    "medium_ukiyo_weighted_v1": {
+        "model_size": "medium",
+        "layer_preset": "standard_weighted",
+
+        "content": {
+            "dataset": "images/VOC2012",
+            "fraction": 0.01,
+        },
+
+        "style": {
+            "dataset": "images/Ukiyo_e",
+            "fraction": 0.2,
+        },
+
+        "curriculum": {
+            "stages": [
+                {"epochs": 4, "lr": low_rate, "style_weight": low_style},
             ]
         }
     }
