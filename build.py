cdd7a9010585cdb8b3b07ccffcd6e9a9b385ccbd
John Blackburn <jackblackburnn@icloud.com>
2025-09-25 12:08:02 -0400

    Clean and refactor scripts for new config & model architectures

--- build.py
+++ build.py
@@ -5,12 +5,12 @@ from style_transfer.train import train_model
 
 if __name__ == '__main__':
 
-    model_name = 'medium_no_style_deep_loss_v1'
+    model_name = 'kanagawa'
     model_config = Models.get(model_name)
 
-    # Device setup
     print("PyTorch version:", torch.__version__)
 
+    # Device setup
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
         device = torch.device("mps")
     elif torch.cuda.is_available():
